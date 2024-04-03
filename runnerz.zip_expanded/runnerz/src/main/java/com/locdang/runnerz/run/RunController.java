c83992b32b24e3fa1df9b32792804d831db0961d
Loc Dang <157776924+locdang99@users.noreply.github.com>
2024-04-03 09:18:33 +0700

    Done API basic CRUD

--- runnerz.zip_expanded/runnerz/src/main/java/com/locdang/runnerz/run/RunController.java
+++ runnerz.zip_expanded/runnerz/src/main/java/com/locdang/runnerz/run/RunController.java
@@ -1,11 +1,20 @@
 package com.locdang.runnerz.run;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/runs")
@@ -31,9 +40,36 @@ public class RunController {
 	List<Run> findAll() {
 		return runRepository.findAll();
 	}
-	
-	@GetMapping("/1")
-	Run findById() {
-		return null;
+
+	@GetMapping("/{id}")
+	Run findById(@PathVariable Integer id) {
+		Optional<Run> run = runRepository.findById(id);
+		if (run.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		return run.get();
+	}
+
+	// Post
+	@PostMapping("")
+	@ResponseStatus(HttpStatus.CREATED)
+	void create(@RequestBody Run run) {
+		runRepository.create(run);
+	}
+
+	// Put
+	@PutMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	void update(@RequestBody Run run, @PathVariable Integer id) {
+		runRepository.update(run, id);
+		;
+	}
+
+	// Delete
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("{id}")
+	void delete(@PathVariable Integer id) {
+		runRepository.delete(id);
+		;
 	}
 }
