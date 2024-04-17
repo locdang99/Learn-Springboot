3794dcc74279b52cb8d9992294935574850c9c0c
Loc Dang <157776924+locdang99@users.noreply.github.com>
2024-04-17 11:29:10 +0700

    [Loc Dang] Done API with connect H2 DB

---
+++ runnerz.zip_expanded/runnerz/src/main/java/com/locdang/runnerz/run/Runs.java
@@ -0,0 +1,7 @@
+package com.locdang.runnerz.run;
+
+import java.util.List;
+
+public record Runs(List<Run> runs) {
+
+}
---
+++ runnerz.zip_expanded/runnerz/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+/*INSERT INTO Run (id, title, started_on, completed_on, miles, location)
+VALUES (1, 'Morning Run', '2024-01-01 01:00:00', '2024-01-01 01:15:00', 5, 'OUTDOOR');*/
